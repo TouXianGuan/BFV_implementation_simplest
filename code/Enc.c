0d8878d8170d0d0a9b4df9f3ab1505a1ad69e3c3
RuiXuan_Zhu <U202215337@hust.edu.cn>
2023-10-18 09:02:28 +0800

    ck0函数

--- code/Enc.c
+++ code/Enc.c
@@ -1,3 +1,15 @@
-Poly* ct0(PolyPairs* pk, Poly* m, int d, int t, int q, Poly* e1, Poly* m) {
+Poly* ct0(PolyPairs* pk, Poly* m, int d, int t, int q, Poly* e1, Poly* u) {
+	//pk0 * u
+	Poly *poly1 = poly_mod_mul(pk->poly0, u, d, q);
 	
+	//pk0 * u + e1
+	Poly *poly2 = poly_mod_add(poly1, e1, d, q);
+	
+	//(q / t) * m
+	Poly *poly3 = poly_mod_coefficient_mul(m, q/t, q);
+	
+	//pk0 * u + e1 + (q / t) * m
+	Poly *poly4 = poly_mod_add(poly2, poly3, d, q);
+	
+	return poly4;	
 }
--- code/Enc.h
+++ code/Enc.h
@@ -1,2 +1,2 @@
 //生成密文第0项 
-Poly* ct0(PolyPairs* pk, Poly* m, int d, int t, int q, Poly* e1, Poly* m);
+Poly* ct0(PolyPairs* pk, Poly* m, int d, int t, int q, Poly* e1, Poly* u);
