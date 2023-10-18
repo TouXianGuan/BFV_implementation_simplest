e9ed0eedfaa1001f40ab827e2e40ec3ce1069104
RuiXuan_Zhu <U202215337@hust.edu.cn>
2023-10-18 18:45:48 +0800

    解密问题解决：原文中q应为896，方可满足q/t = 128

--- code/main.c
+++ code/main.c
@@ -9,21 +9,18 @@
 #include "Dec.h"
 
 int c1[16] = {252, -113, -234, 110, 377, -281, -158, 26, 430, -41, -142, -83, 86, -32, -431, -285};
-int c2[9] = {3, 0, 0, 0, 0, 0, 0, 0, -3};
-int c3[16] = {4, -6, 2, -3, -3, -4, 5, 4, 4, 1, 3, -4, -1, 3, -2, -5};
+int c2[9] = {3, 0, 2, 0, 10, 0, 0, 0, -3};
+int c3[16] = {4, -6, 2, -3,-3, -4, 5, 4, 4, 1, 3, -4, -1, 3, -2, -5};
 int c4[16] = {2, -2, -4, 1, -2, 2, -3, -4, 4, -1, 2, 5, 0, -4, 2, -7};
 int c5[15] = {1, 0, 0, -1, 0, -1, 0, 0, -1, 0, 0, 0, 1, 1, 1};
 int c6[16] = {84, -60, -282, 186, 322, -138, 70, 52, 107, -212, -369, 447, -229, -393, -256, 42};
 int c7[16] = {393, 7, -12, -2, -3, -13, 10, 9, -380, 19, -23, -32, 22, 17, -2, 13};
-int c8[] = {-1, 1, 1, 0, -1, 0, 1, 0, 1, -1, 0, -1, -1, -1, 0, 1};
-int c9[] = {42, 23, 144, -247, -258, -201, 184, 5, 115, 252, -238, -392, -249, 13, -53, 217};
-int c10[] = {380, -91, -26, -20, 68, -332, 225, 386, -330, 56, -24, 350, 270, -12, 225, 25};
 int i, len;
 
 int main(int argc, char *argv[]) {
 	srand(time(NULL) * 10000); 
 	
-	/*
+	
 	len = sizeof(c1) / sizeof(int);
 	Poly* poly1 = poly_create(len - 1);
 	for (i=0; i<len; i++) {
@@ -83,46 +80,19 @@ int main(int argc, char *argv[]) {
 	printf("sk\t\t");
 	poly_print(Sk, 1); 
 	
-	PolyPairs *Pk = pk(Sk, 16, 874);
+	PolyPairs *Pk = pk(Sk, 16, 896);
 	printf("pk\n");
 	polypairs_print(Pk, 1); 
 	printf("\n");
 	
-	PolyPairs *Ct = ct(Pk, poly2, 16, 7, 874);
+	PolyPairs *Ct = ct(Pk, poly2, 16, 7, 896);
 	printf("Ct\n");
 	polypairs_print(Ct, 1); 
 	printf("\n");
-	*/
 	
-	len = sizeof(c8) / sizeof(int);
-	Poly* poly8 = poly_create(len - 1);
-	for (i=0; i<len; i++) {
-		poly_term(poly8, c8[i], i);
-	}
-	printf("多项式八\t");
-	poly_print(poly8, 1);
-	
-	len = sizeof(c9) / sizeof(int);
-	Poly* poly9 = poly_create(len - 1);
-	for (i=0; i<len; i++) {
-		poly_term(poly9, c9[i], i);
-	}
-	printf("多项式九\t");
-	poly_print(poly9, 1);
-	
-	len = sizeof(c10) / sizeof(int);
-	Poly* poly10 = poly_create(len - 1);
-	for (i=0; i<len; i++) {
-		poly_term(poly10, c10[i], i);
-	}
-	printf("多项式十\t");
-	poly_print(poly10, 1);
-	
-	Poly* poly11 = poly_mul(poly10, poly8);
-	Poly* poly12 = poly_add(poly11, poly9);
-	Poly* poly13 = poly_mod_poly(poly12, 16);
-	//Poly* poly14 = poly_mod_coefficient(poly13, 874);
-	poly_print(poly13, 1); 
+	Poly* M = m_dec(Ct, Sk, 16, 7, 896);
+	printf("M\t\t");
+	poly_print(M, 1);
 	
 	return 0;
 }
